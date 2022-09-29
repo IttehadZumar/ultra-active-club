ba0911c4f68f91ea6b85bbb7b3de843366d26e7b
ittehad-abdullah <ittehad15-3530@diu.edu.bd>
2022-09-29 23:15:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { toast, ToastContainer } from 'react-toastify';
 import './App.css';
+import Meditationbody from './components/Meditationbody/Meditationbody';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => {
+    toast.success('🧘 Meditation Completed!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Meditationbody notify={notify}></Meditationbody>
     </div>
+
   );
 }
 
